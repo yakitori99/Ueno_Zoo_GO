6a283d05606332b07f4462f2eb6978cef26feb1c
yakitori99 <t2.akg3@gmail.com>
2021-07-01 01:56:25 +0900

    herokuデプロイ用に、sshをコメントアウト。環境変数PORTのポート番号でapp.run。

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Starting SSH ..."
-service ssh start
+# del for heroku
+# echo "Starting SSH ..."
+# service ssh start
 
 python /code/runserver.py
--- runserver.py
+++ runserver.py
@@ -3,7 +3,17 @@ This script runs the flaskwebapp application using a development server.
 """
 
 from flaskwebapp import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80, threaded=True)  # debug用
+    # app.run(debug=True, host='0.0.0.0', port=80, threaded=True)  # debug用
     # app.run(debug=False, host='0.0.0.0', port=80, threaded=True)  # 本番用
+    
+    ## heroku本番用
+    # 環境変数'PORT'があれば、そのポートでrun
+    port_env = os.getenv('PORT')
+    if port_env is None:
+        port_num = 80
+    else:
+        port_num = int(port_env)
+    app.run(debug=False, host='0.0.0.0', port=port_num, threaded=True)
