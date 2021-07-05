2b53c3390c34eeca4eaaea802031984cb8eb74f3
yakitori99 <t2.akg3@gmail.com>
2021-07-05 11:21:59 +0900

    環境変数'PORT'の有無でポートを切り替えるよう変更

--- runserver.py
+++ runserver.py
@@ -6,14 +6,13 @@ from flaskwebapp import app
 import os
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=80, threaded=True)  # debug用
-    # app.run(debug=False, host='0.0.0.0', port=80, threaded=True)  # 本番用
-    
-    ## heroku本番用
-    # 環境変数'PORT'があれば、そのポートでrun
+    # 環境変数'PORT'があればherokuとみなし、そのポートでrun
     port_env = os.getenv('PORT')
     if port_env is None:
+        # debug用
         port_num = 80
+        app.run(debug=True,  host='0.0.0.0', port=port_num, threaded=True)
     else:
+        # heroku用
         port_num = int(port_env)
-    app.run(debug=False, host='0.0.0.0', port=port_num, threaded=True)
+        app.run(debug=False, host='0.0.0.0', port=port_num, threaded=True)
